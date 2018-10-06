429bfd5b34cc2edb0a355332ca17160ce406fd8e
Sung Bae <singsang2@gmail.com>
2018-10-06 20:19:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    false
+  elsif number > 1
+    for i in 2..(number - 1)
+      if number % i == 0
+        return false
+      end
+    end 
+    return true
+  end
+end
