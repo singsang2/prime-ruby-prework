8d1f9aaf0effdfb29377ab77a955d9b89fd6fd83
Sung Bae <singsang2@gmail.com>
2018-10-09 14:55:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   if number < 2
     false
   else  
-    for i in 2..(floor(number**0.5))
+    for i in 2..((number**0.5).round)
       if number % i == 0
         return false
       end
